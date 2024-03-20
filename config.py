17b8a936ba0912267335a5fb08f2d6214af8e4cc
huntergregory <42728408+huntergregory@users.noreply.github.com>
2024-03-20 14:48:11 +0100

    chore: clarifications to config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 ## NOTE: you can also set environmental variables instead of modifying this file
 ## This file takes precedence over environmental variables
 
-# must either 1) specify OpenAI API key
+# must choose ONE option: 
+# 1) specify OpenAI API key OR
+# 2) specify local model
 OAI_API_KEY=None # replace with string
-# or 2) specify local model
-LOCAL_LLM=None # replace with path to model
+LOCAL_LLM=None # 'mistral-7b-openorca' # replace with model name from llm utility
 
 # optional: for Advent of Code, this key allows automation of downloading input and submitting answers.
 # See README on how to get this key
